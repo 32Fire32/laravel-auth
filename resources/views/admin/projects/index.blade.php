b46e60d31addaa8e1a71e36d484994581464a795
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-25 17:29:33 +0100

    stampa la index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin.admin')
+
+@section('content')
+    <h1>Lista Progetti:</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome Progetto</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Cliente/th>
+                <th scope="col">Data creazione</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->name_project }}</td>
+                    <td>{{ $project->summary }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td>{{ $project->client }}</td>
+                    <td>{{ $project->shipped_at }}</td>
+                    <td>...</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rotte pubbliche
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Tutte le rotte protette da autenticazione
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    Route::resource('projects', ProjectController::class);
+
 });
 
 require __DIR__.'/auth.php';
